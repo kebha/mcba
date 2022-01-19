234edb8f730a139fa5a27c4d8013dfff3ab31f28
Jeri <kebhajerin@gmail.com>
2022-01-20 01:11:12 +1100

    customerID and name is stored in session

--- s3844648-a2/Controllers/LoginController.cs
+++ s3844648-a2/Controllers/LoginController.cs
@@ -20,7 +20,8 @@ public class LoginController : Controller
     public async Task<IActionResult> Login(string loginID, string password)
     {
         var login = await _context.Logins.FindAsync(loginID);
-        if(login == null || string.IsNullOrEmpty(password) || !PBKDF2.Verify(login.PasswordHash, password))
+        var customer = await _context.Customers.FindAsync(login.CustomerID);
+        if (login == null || string.IsNullOrEmpty(password) || !PBKDF2.Verify(login.PasswordHash, password))
         { 
             ModelState.AddModelError("LoginFailed", "Login failed, please try again.");
             return View(new Login { LoginID = loginID });
@@ -28,7 +29,7 @@ public class LoginController : Controller
 
         // Login customer.
         HttpContext.Session.SetInt32(nameof(Customer.CustomerID), login.CustomerID);
-        HttpContext.Session.SetString(nameof(Customer.Name), login.Customer.Name);
+        HttpContext.Session.SetString(nameof(Customer.Name), customer.Name);
 
         return RedirectToAction("Index", "Customer");
     }
