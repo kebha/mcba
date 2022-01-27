55cd033c50c6823f4b68b1f589656dce7b7999b2
Jerin Kebha <S3844648@student.rmit.edu.au>
2022-01-27 13:58:28 +1100

    fixed contraint for State field

--- s3844648-a2/Models/Customer.cs
+++ s3844648-a2/Models/Customer.cs
@@ -32,7 +32,7 @@ public class Customer
     [StringLength(40)]
     public string? Suburb { get; set; }
 
-    [StringLength(3)]
+    //[StringLength(3)]
     public State? State { get; set; }
 
     [JsonProperty("PostCode")]
